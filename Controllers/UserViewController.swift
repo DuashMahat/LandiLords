2acb7bd90a5c892efa9c6991f65052a79ff931db
Duale Abdullahi <abdull53@msu.edu>
2020-08-22 15:23:37 -0500

    Refactored some code

--- Controllers/UserViewController.swift
+++ Controllers/UserViewController.swift
@@ -37,7 +37,6 @@ extension UserViewController  {
         picker.delegate = self
         picker.allowsEditing = true
         picker.sourceType = .photoLibrary
-        present (picker , animated:  true , completion:  nil)
     }
 }
 
@@ -53,32 +52,15 @@ extension UserViewController {
         }))
 
         alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
+    
+    
 
 }
 
 extension UserViewController : UIImagePickerControllerDelegate , UINavigationControllerDelegate {
    
-    func  openCamera()   {
-        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
-         {
-            picker.sourceType = UIImagePickerController.SourceType.camera
-             picker.allowsEditing = true
-             self.present(picker, animated: true, completion: nil)
-         }
-         else
-         {
-             let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
-             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-             self.present(alert, animated: true, completion: nil)
-         }
-       }
-       
-   func openGallary() {
-           picker.sourceType = UIImagePickerController.SourceType.photoLibrary
-           picker.allowsEditing = true
-           self.present(picker, animated: true, completion: nil)
-   }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
            guard let imageEdited = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
@@ -86,6 +68,27 @@ extension UserViewController : UIImagePickerControllerDelegate , UINavigationCon
                return}
            profileimage.image = imageEdited
            dismiss(animated: true, completion: nil)
+    }
+    
+    func  openCamera()   {
+         if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
+          {
+             picker.sourceType = UIImagePickerController.SourceType.camera
+              picker.allowsEditing = true
+              self.present(picker, animated: true, completion: nil)
+          }
+          else
+          {
+              let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+              alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+              self.present(alert, animated: true, completion: nil)
+          }
+        }
+        
+    func openGallary() {
+            picker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            picker.allowsEditing = true
+            self.present(picker, animated: true, completion: nil)
     }
 }
 
